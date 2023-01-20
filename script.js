0f44d423b8cd57341ada4945ed02ca0b79ceff51
DavideFolk <107867374+DavideFolk@users.noreply.github.com>
2023-01-20 15:31:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+let arrNomi = [];
+
+let arrCamere = ['camera1', 'camera2', 'camera3', 'camera4'];
+
+let nCamere = 1;
+
+let arrTempCamere = [];
+
+function creaArrCamere(nCamere) {
+	if (nCamere === 1) arrTempCamere = [[]];
+	if (nCamere === 2) arrTempCamere = [[], []];
+	if (nCamere === 3) arrTempCamere = [[], [], []];
+	if (nCamere === 4) arrTempCamere = [[], [], [], []];
+}
+
+$('#nomeBtn').click(function (e) {
+	e.preventDefault();
+	let nome = $('#nomeInserito').val();
+	if (nome) {
+		arrNomi.push(nome);
+
+		$('#elencoNomi').append(`<p>${nome}</p>`);
+
+		$('#nomeInserito').val('');
+	}
+});
+
+$(document).on('input', '#cameraRange', function () {
+	nCamere = Number($('#cameraRange').val());
+	arrCamere.forEach((element, index) => {
+		if (nCamere >= index + 1) {
+			$(`#${element}`).show();
+		} else {
+			$(`#${element}`).hide();
+		}
+	});
+	creaArrCamere(nCamere);
+});
+
+$('#assegna').click(function (e) {
+	$('.nomeElenco').remove();
+	let arrNomiRandom = _.shuffle(arrNomi);
+	arrNomiRandom.forEach((nome) => {
+		arrTempCamere[0].push(nome);
+		arrTempCamere = _.orderBy(arrTempCamere, [(i) => i.length], []);
+	});
+	console.log(arrTempCamere);
+	arrTempCamere.forEach((element, index) => {
+		element.forEach((elem) => {
+			$(`#camera${index + 1}`).append(
+				`<p class="nomeElenco">${elem}</p>`
+			);
+		});
+	});
+	creaArrCamere(nCamere);
+});
+
+//arrTemp = _.orderBy(arrTemp, [(i) => i.length], []);
+//_.random(1, nCamere)
